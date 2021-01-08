0abec012e44fc3e975adfb9d7db9c0e97e7f2e96
Joel <joelcox.joel@gmail.com>
2021-01-08 18:16:34 +0000

    Initial Setup of the project

---
+++ Arkhana/Arkhana/Arkhana.cpp
@@ -0,0 +1,10 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main() {
+	RenderWindow* window = new RenderWindow(VideoMode(960, 960), "Card games or something idk");
+
+	return 0;
+}
